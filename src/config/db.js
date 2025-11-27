c353e1a7417b76c588a52a916f309e2c96ec70f3
vendasindoor2026 <vendasindoor2026@gmail.com>
2025-11-27 04:06:06 -0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
   ssl: {
     rejectUnauthorized: false
   }
