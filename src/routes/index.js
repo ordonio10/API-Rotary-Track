884b844b246743f8c0429fc67f5b409e42958681
vendasindoor2026 <vendasindoor2026@gmail.com>
2025-11-27 03:42:22 -0300

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import clienteRoutes from "./clientes.js";
 import veiculoRoutes from "./veiculos.js";
+import posicoes from "./posicoes.js"; // ADICIONAR
 
 const router = Router();
 
 router.use("/clientes", clienteRoutes);
 router.use("/veiculos", veiculoRoutes);
+router.use("/posicoes", posicoes); // ADICIONAR
 
 export default router;
